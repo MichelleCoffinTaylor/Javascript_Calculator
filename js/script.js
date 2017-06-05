d378732093bc8cb513044fd438650d6b2497e10b
michelle jasmin coffin-taylor <845860590@ken20-202-11.local>
2017-06-06 11:53:33 +1200

    Started Calculators functions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
 // Javascript
 
-function AddNumber(num1, num2);
-	
+var DisplayedEquation = document.getElementsByClassName("box");
+var PlusDiv = document.getElementById("add");
+
+var equationArray =[];
+
+window.onload = function() {
+        var numberButtons = document.getElementsByClassName('number');
+        for(var i = 0; i < numberButtons.length; i++) {
+           var numberButton = numberButtons[i];
+           numberButton.onclick = function() {
+           		DisplayedEquation[0].innerText += this.innerText;
+
+            }
+        }
+    };
+
+PlusDiv.onclick = function() {
+
+if (equationArray == "") {
+		equationArray.push(DisplayedEquation[0].innerText);
+           		DisplayedEquation[0].innerText = "";	
+			}else{
+				DisplayedEquation[0].innerText = "Loop to add all variables in array";
+			}
+           		
+};
